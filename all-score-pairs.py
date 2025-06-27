ea504279e5e252281a5a1abe2e411ee98280e136
Dylan Thinnes <dylan.thinnes@protonmail.com>
2025-06-27 02:52:00 +0100

    Turn all-score-pairs and download into standalone stdin-driven programs

--- all-score-pairs.py
+++ all-score-pairs.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import csv
+import sys
 
-with open('all-scores.csv') as fd:
+with open(sys.argv[1]) as fd:
     rows = [row for row in csv.reader(fd)]
     rows = rows[1:] # drop header
     scores = [(int(row[2]), int(row[3])) for row in rows]
